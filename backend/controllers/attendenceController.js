e99ef0923a97f1887f0c65476835138d84e02c08
Daniel Andrade <danielbarretoandrade@gmail.com>
2023-09-01 19:17:58 -0300

    adicionando check de presenca ja existente

--- backend/controllers/attendenceController.js
+++ backend/controllers/attendenceController.js
@@ -28,12 +28,44 @@ const registerAttendence = asyncHandler( async (req, res) => {
         throw new Error("Member does not exists")
     };
 
+    const checkAttendence = await Attendence.findOne({
+        name,
+        date
+    })
+
+    if(checkAttendence){
+        res.status(400)
+        throw new Error("Attendence already exists")
+    };
+
     // TODO: validar se essa mesma presenca já foi criada 
 
 
     // Create new presence
     
     const memberId = member._id;
+
+    /*
+            const Product = model('Product', productSchema);
+
+        // Example query to find products with specific conditions
+        const searchCriteria = {
+        name: 'Product Name', // Search by product name
+        category: 'Electronics', // Search by product category
+        price: { $gte: 100, $lte: 500 }, // Search by price range ($gte: greater than or equal to, $lte: less than or equal to)
+        };
+
+        Product.find(searchCriteria, (err, products) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Matching Products:', products);
+        }
+        });
+    
+    
+    
+    */
 
     const attendance = await Attendence.create({
         member: memberId,
