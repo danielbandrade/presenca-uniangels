22b0c596730d943332726b12af6f39233efc7741
Daniel Andrade <danielbarretoandrade@gmail.com>
2023-09-02 12:59:20 -0300

    aprendendo sobre botao de selecao no front

--- frontend/front-lista-presenca/src/pages/attendence.js
+++ frontend/front-lista-presenca/src/pages/attendence.js
@@ -8,6 +8,62 @@ const inter = Inter({ subsets: ['latin'] })
 // TODO: criar todos os membros no backend
 
 
+export async function getServerSideProps() {
+    
+  // Busca os membros
+
+  const response = await fetch('http://localhost:5000/api/members/getmembers');
+  const data = await response.json();
+
+  return {
+      props: {
+          data,
+        },
+  };
+}
+
+
+
+const registerAttendence = () => {
+  
+  // TODO preciso capturar os dados inseridos na interface e enviar da maneira adequada para o backend
+  // posso começar garantindo que os dados estão chegando na função 
+
+  
+
+
+  /*
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    
+    try {
+        const response = await fetch('http://localhost:5000/api/attendences/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+  
+        if (response.ok) {
+          // Handle successful login response here
+          router.push('/attendence');
+        } else {
+          // Handle login error response here
+          console.log('Login failed!');
+        }
+      } catch (error) {
+        // Handle error here
+        console.error('Error:', error);
+      }
+      */
+
+  };
+
+
 
 
 export default function Attendence({ data }){
@@ -20,22 +76,39 @@ export default function Attendence({ data }){
       <h1>Estes são os membros</h1>
       <ul>
           {data.selectedObjects.map((item, index) => (
-            <li key={index}>Nome:{item.name}, Criado em: {item.createdAt} </li>
+            
+            <li key={index}>
+              Nome:{item.name}, Criado em: {item.createdAt} 
+            </li>
+          
+            
           ))}
-        </ul>
+      </ul>
+
+      <div>
+
+
+      <h2>Multi-Select List</h2>
+      <ul>
+        {data.selectedObjects.map((item, index) => (
+          <li key={index}>
+            <label>
+              {item.name}
+              <input
+                type="checkbox"
+                value={item}
+              />
+            </label>
+          </li>
+        ))}
+      </ul>
+      <p>Selected Items: </p>
+    </div>      
+
+
+
     </main>
   )
 }
 
-  
-export async function getServerSideProps() {
-    
-    const response = await fetch('http://localhost:5000/api/members/getmembers');
-    const data = await response.json();
 
-    return {
-        props: {
-            data,
-          },
-    };
-}
