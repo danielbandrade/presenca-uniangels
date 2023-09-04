113bfd4df29f478fb83346b0536e7b03e9de691a
Daniel Andrade <danielbarretoandrade@gmail.com>
2023-09-04 01:45:07 -0300

    ajustando fortemente o css

--- frontend/front-lista-presenca/src/pages/attendence.js
+++ frontend/front-lista-presenca/src/pages/attendence.js
@@ -1,12 +1,12 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
-
 // TODO: fazer a tela de registro de presenca
 // TODO: criar todos os membros no backend
+// TODO entender pq a lista dos membros selecionados nao eh exibida
+// TODO ajustar o date time para exibir somente a data
+// documentacao css https://v1.tailwindcss.com/components/buttons
 
 
 export async function getServerSideProps() {
@@ -34,7 +34,8 @@ const registerAttendence = async() => {
   console.log("increment like count")
 
   try {
-    const response = await fetch('http://localhost:5000/api/attendences/register', {
+    const response = await fetch('http://localhost:5000/api/attendences/register', 
+    {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -83,29 +84,35 @@ export default function Attendence({ data }){
   }; 
 
   return (
-    <main
-      className={`flex min-h-screen flex-col justify-between p-24 ${inter.className}`}>
-      <h1>Estes são os membros</h1>
-      <ul>
-          {data.selectedObjects.map((item, index) => (
-            
-            <li key={index}>
-              {item.name}, Criado em: {item.createdAt} 
-            </li>
+    <main>
+        
+      <h1 className=" flex text-1xl font-bold ">Estes são os membros</h1>
+      
+      <p>
+        <ul>
+            {data.selectedObjects.map((item, index) => (
+              
+              <li key={index}>
+                <div >{item.name} </div> 
+                
+                <div> Criado em: {item.createdAt} </div>
+              </li>
           
-            
-          ))}
-      </ul>
+            ))}
+        </ul>
+      </p>
 
       <div>
 
-      <h2>Esta é a lista de presença dos membros</h2>
+      <h2 className=" my-2 text-1xl font-bold ">Lista de presença dos membros</h2>
+      <form className=" my-2">
       <ul>
         {data.selectedObjects.map((item, index) => (
           <li key={index}>
             <label>
               {item.name}
               <input
+                class="appearance-none border-2  rounded mx-2 py-2 px-2 leading-tight checked:bg-gray-200"
                 type="checkbox"
                 value={item.name}
                 checked={selectedItems.includes(item)}
@@ -115,12 +122,13 @@ export default function Attendence({ data }){
           </li>
         ))}
       </ul>
-      <p>Selected Items: {selectedItems.join(', ')}</p>
+      <p>Membros presentes: {selectedItems.join(', ')}</p>
 
       
-    <button onClick={registerAttendence}>Registrar Presença</button>     
+      <button class="my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={registerAttendence} >Registrar Presença</button>   
+    </form>
     </div>      
-    
+
     </main>
   )
 }
