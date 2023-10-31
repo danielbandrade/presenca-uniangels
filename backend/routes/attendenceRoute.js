3dcbccebab5863e4522c9494b481037a0eca9e80
Daniel Andrade <danielbarretoandrade@gmail.com>
2023-10-31 11:05:00 -0300

    adicionando a validacao de token em outros endpoints

--- backend/routes/attendenceRoute.js
+++ backend/routes/attendenceRoute.js
@@ -8,12 +8,13 @@ const {
     deleteAttendenceLog,
     calculateMemberAttendence,
 } = require("../controllers/attendenceController");
+const protect = require("../middleWare/authMiddleware");
 
 
 router.post("/register", registerAttendence);
-router.get("/getattendencelog", getAttendenceLog);
+router.get("/getattendencelog", protect, getAttendenceLog);
 router.post("/deleteattendencelog", deleteAttendenceLog);
-router.get("/calculatememberattendence", calculateMemberAttendence);
+router.get("/calculatememberattendence", protect ,calculateMemberAttendence);
 
 
 
